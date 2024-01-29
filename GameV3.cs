57e5b3002e85d59909a499dd7bd2d69d72d76bd9
Curse <37249669+Curscs@users.noreply.github.com>
2024-01-29 11:24:37 +0000

    Update GameV3.cs

--- GameV3.cs
+++ GameV3.cs
@@ -22,133 +22,126 @@ public class Pet
     public double Attack { get; set; }
     public double Health { get; set; }
 
-public class Mobs
-{
-    public Dictionary<string, Dictionary<string, (Pet, int) >> mobs = new Dictionary<string, Dictionary<string, (Pet, int)>>
+    public class Mobs
     {
+        public Dictionary<string, Dictionary<string, (Pet, int)>> mobs = new Dictionary<string, Dictionary<string, (Pet, int)>>
+        {
 
-    };
+        };
 
-}
+    }
 
-public class Eggs
-{
-    public Dictionary<string, Dictionary<string, double>> eggs = new Dictionary<string, Dictionary<string, double>>
+    public class Eggs
     {
-        {
-            "Common Egg", new Dictionary<string , double>
-            {
-                {"Doggy", 30.0},
-                {"Cat", 30.0},
-                {"Demon", 0.9}
-            }
-
-        },
-        {
-            "Rare Egg", new Dictionary<string , double>
-            {
-                {"Doggy", 30.0},
-                {"Cat", 30.0},
-                {"Demon", 0.9}
-            }
-        },
-    };
-
-    public void OpenEgg(string eggname, Player player)
+        public Dictionary<string, Dictionary<string, (Pet, int)>> eggs = new Dictionary<string, Dictionary<string, (Pet, int)>>
     {
-        if (eggs.ContainsKey(eggname))
-        {
-            //retrieve data from dictionary
-            Dictionary<string, double> eggContents = eggs[eggname];
-
-
-            //Calculate total pet chance
-            double TotalChance = 0;
-            foreach (var petChance in eggContents.Values)
             {
-                TotalChance += petChance;
-            }
-
-            //Generates random number and then * it by the totalchance 
-            double randomnumber = new Random().NextDouble() * TotalChance;
-
-            foreach (var Petname in eggContents)
-            {
-                if (randomnumber <= Petname.Value)
+                "Common Egg", new Dictionary<string, (Pet, int)>
                 {
-                    player.AddPet(Petname.Key, player);
-                    Console.WriteLine($"You have hatched {Petname.Key}");
-                    break;
+                    {"Doggy", (new Pet { Name = "Doggy", Attack = 3, Health = 20}, 30)},
+                    {"Cat", (new Pet { Name = "Doggy", Attack = 3, Health = 20}, 30)},
+                    {"Bear", (new Pet { Name = "Bear", Attack = 3, Health= 20}, 10)},
+                    
+                }
+            }
+    };
+
+        public void OpenEgg(string eggname, Player player)
+        {
+            if (eggs.ContainsKey(eggname))
+            {
+                //retrieve data from dictionary
+                Dictionary<string, (Pet, int)> eggContents = eggs[eggname];
+
+
+                //Calculate total pet chance
+                double TotalChance = 0;
+                foreach (var petChance in eggContents.Values)
+                {
+                    TotalChance += petChance;
                 }
 
-                randomnumber -= Petname.Value;
+                //Generates random number and then * it by the totalchance 
+                double randomnumber = new Random().NextDouble() * TotalChance;
+
+                foreach (var Petname in eggContents)
+                {
+                    if (randomnumber <= Petname.Value)
+                    {
+                        player.AddPet(Petname.Key, player);
+                        Console.WriteLine($"You have hatched {Petname.Key}");
+                        break;
+                    }
+
+                    randomnumber -= Petname.Value;
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Egg with name '{eggname}' not found.");
             }
         }
-        else
+
+    }
+
+    public class Program
+    {
+        public static void Main()
         {
-            Console.WriteLine($"Egg with name '{eggname}' not found.");
+            //class info retrieve
+            Eggs eggInstance = new Eggs();
+            Player player = new Player();
+            //functions
+            ConsoleGame.Startup(player);
         }
     }
 
-}
-
-public class Program
-{
-    public static void Main()
+    public class ConsoleGame
     {
-        //class info retrieve
-        Eggs eggInstance = new Eggs();
-        Player player = new Player();
-        //functions
-        ConsoleGame.Startup(player);
-    }
-}
-
-public class ConsoleGame
-{
-    public static void Startup(Player player)
-    {
-        Console.WriteLine("Welcome to Console World");
-        if (player.username == "")
+        public static void Startup(Player player)
         {
-            GetUsername(player);
+            Console.WriteLine("Welcome to Console World");
+            if (player.username == "")
+            {
+                GetUsername(player);
+            }
         }
-    }
-    public static void GetUsername(Player player)
-    {
-        Console.Write("Enter your username: ");
-        player.username = Console.ReadLine();
-    }
-    public static void OptionsDisplay(ref int choice)
-    {
-        Console.WriteLine("Here are some of your options:");
-        Console.WriteLine("1 - Go on adventure");
-        Console.WriteLine("2 - Open shop");
-        Console.WriteLine("3 - Exit the game");
-        Console.Write("Enter your choice: ");
-        choice = int.Parse(Console.ReadLine());
-    }
-
-    public static void ActOnChoice(int choice)
-    {
-        switch (choice)
+        public static void GetUsername(Player player)
         {
-            case 1: Console.WriteLine();
-                break;
-            case 2:
-                Console.WriteLine();
-                break;
-            case 3:
-                Console.WriteLine();
-                break;
+            Console.Write("Enter your username: ");
+            player.username = Console.ReadLine();
+        }
+        public static void OptionsDisplay(ref int choice)
+        {
+            Console.WriteLine("Here are some of your options:");
+            Console.WriteLine("1 - Go on adventure");
+            Console.WriteLine("2 - Open shop");
+            Console.WriteLine("3 - Exit the game");
+            Console.Write("Enter your choice: ");
+            choice = int.Parse(Console.ReadLine());
+        }
+
+        public static void ActOnChoice(int choice)
+        {
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine();
+                    break;
+                case 2:
+                    Console.WriteLine();
+                    break;
+                case 3:
+                    Console.WriteLine();
+                    break;
+
+            }
+        }
+
+        public static void GoOnAdventure()
+        {
 
         }
-    }
 
-    public static void GoOnAdventure()
-    {
 
     }
-
-
-}
