4b548caa4dc5b772239abc1855d8cd566c401b78
Curse <37249669+Curscs@users.noreply.github.com>
2024-02-08 01:19:54 +0000

    Update GameV3.cs

--- myOwnPractise/GameV3.cs
+++ myOwnPractise/GameV3.cs
@@ -1,152 +1,238 @@
-using System;
-using System.Collections.Generic;
+/* 
+ useful scripts for me to remember:
 
+ 1) foreach (var (x, index) in x.value.Select((value, i) => (value, i + 1)))
+ 2) (!int.TryParse(Console.ReadLine(), out choice) || choice < 1 || choice > 3)
+ 
+ 
+ 
+ 
+ */
 public class Player
 {
     public string username { get; set; } = "";
-    public List<string> Items { get; set; } = new List<string>();
-    public List<string> Pets { get; set; } = new List<string>();
+    public int coins { get; set; } = 0;
+    public List<string> weaponInventory = new List<string>();
+    public List<string> items = new List<string>();
 
-    public void AddPet(string PetName, Player player)
+    public void AddItem(string itemName)
     {
-        player.Pets.Add(PetName);
+        items.Add(itemName);
     }
 }
 
-public class Pet
+public class Weapon
 {
-    public string Name { get; set; }
+    public string name { get; set; }
+    public string rarity { get; set; }
+    public int damage { get; set; }
+}
 
-    public string Rarity { get; set; }
-    public double Attack { get; set; }
-    public double Health { get; set; }
-
-    public class Mobs
+public class Crate
+{
+    private static readonly Dictionary<string, Dictionary<string, (Weapon, int)>> crate = new Dictionary<string, Dictionary<string, (Weapon, int)>>
     {
-        public Dictionary<string, Dictionary<string, (Pet, int)>> mobs = new Dictionary<string, Dictionary<string, (Pet, int)>> { };
-    }
+        {
+            //weapon (name , rarity , damage) item1
+            //chance item2
 
-    public class Eggs
+            "Common Crate", new Dictionary<string, (Weapon, int)>
+            {
+                {"Wooden Sword", (new Weapon { name = "Wooden Sword", rarity = "Common", damage = 5}, 30)},
+                {"Long wooden sword", (new Weapon { name = "Long Wooden Sword", rarity = "Rare", damage = 10}, 10)}
+            }
+        }
+    };
+
+    private static readonly Dictionary<string,(int, string)> crateStat = new Dictionary<string, (int, string)>
     {
-        public Dictionary<string, Dictionary<string, (Pet, int)>> eggs = new Dictionary<string, Dictionary<string, (Pet, int)>>
+        {"Common Crate", (300, "Coins")} //parameter 1 - crate name , parameter 2 - price, parameter 3 - currency
+    };
+
+    static Random random = new Random();
+
+    public static IEnumerable<Weapon> GetAllWeapons()
+    {
+        foreach (var crate in  crate) 
         {
+            foreach (var weapon in crate.Value)
             {
-                "Common Egg", new Dictionary<string, (Pet, int)>
-                {
-                    {"Doggy", (new Pet { Name = "Doggy", Rarity = "Secret", Attack = 3, Health = 20}, 30)},
-                    {"Cat", (new Pet { Name = "Cat", Rarity = "Common", Attack = 3, Health = 20}, 30)},
-                    {"Bear", (new Pet { Name = "Bear", Rarity = "Rare", Attack = 3, Health= 20}, 10)},
-                }
+                yield return weapon.Value.Item1;
             }
-        };
-
-        static Random random = new Random();
-
-        public void OpenEgg(string eggname, Player player)
-        {
-            if (eggs.ContainsKey(eggname))
-            {
-                // retrieve data from dictionary
-                Dictionary<string, (Pet, int)> eggContents = eggs[eggname];
-
-                // Calculate total pet chance
-                double TotalChance = 0;
-                foreach (var petTuple in eggContents.Values)
-                {
-                    double petChance = petTuple.Item2;
-                    TotalChance += petChance;
-                }
-
-                // Generates random number and then * it by the totalchance 
-                double randomnumber = random.NextDouble() * TotalChance;
-
-                foreach (var Petname in eggContents.Values)
-                {
-                    if (randomnumber <= Petname.Item2)
-                    {
-                        if (Petname.Item1.Rarity == "Secret")
-                        {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            player.AddPet(Petname.Item1.Name, player);
-                            Console.WriteLine($"You have hatched {Petname.Item1.Rarity} {Petname.Item1.Name}");
-                            Console.ForegroundColor = ConsoleColor.Gray;
-                            break;
-
-                        }
-                        else
-                        {
-                            player.AddPet(Petname.Item1.Name, player);
-                            Console.WriteLine($"You have hatched {Petname.Item1.Rarity} {Petname.Item1.Name}");
-                            break;
-                        }
-                    }
-
-                    randomnumber -= Petname.Item2;
-                }
-            }
-            else
-            {
-                Console.WriteLine($"Egg with name '{eggname}' not found.");
-            }
+            
         }
     }
 
-    public class Program
+    public static IEnumerable<(string Egg, int Index)> GetAllCrates()
     {
-        public static void Main()
+        foreach (var (egg,index) in crate.Keys.Select((value, i) => (value, i + 1)))
         {
-            // class info retrieve
-            Eggs eggInstance = new Eggs();
-            Player player = new Player();
-            // functions
-            ConsoleGame.Startup(player);
-
-            for (int i = 0; i < 30; i++)
-            {
-                eggInstance.OpenEgg("Common Egg", player);
-            }
+            yield return (Egg: egg, Index: index);
         }
     }
 
-    public class ConsoleGame
+    public static bool CheckIfPlayerHasEnoughCurrency(Player playerInstance, int price)
     {
-        public static void Startup(Player player)
+        if (playerInstance.coins >= price)
         {
-            Console.WriteLine("Welcome to Console World");
-            if (player.username == "")
+            return true;
+        }
+        return false;
+    }
+
+    public static void BuyCrate(int choice)
+    {
+        int crateprice;
+        foreach (var (price,index) in crateStat.Select((value, i) => (value, i + 1)))
+        {
+            if choice 
+        }
+        if (Player.CheckIfPlayerHasEnoughCurrency())
+        {
+
+        }
+    }
+
+    public static void OpenCrate(string crateName)
+    {
+        if (crate.ContainsKey(crateName))
+        {
+            //access crate data
+            Dictionary<string,(Weapon, int)> crateData = crate[crateName];
+
+            //total weapon chances
+            double totalChance = crateData.Values.Sum(weaponData => weaponData.Item2);
+            //generate random number * by total chance
+            double randomNum = random.NextDouble() * totalChance;
+            double cumulativeChance = 0;
+            foreach (var (weapon, chance) in crateData.Values)
             {
-                GetUsername(player);
+                cumulativeChance += chance;
+                if (randomNum <= cumulativeChance) 
+                {
+                    Console.WriteLine($"you have unboxed {Utils.articleCheck(weapon.rarity)} {weapon.name}");
+                    return;
+                }
             }
         }
-        public static void GetUsername(Player player)
+    }
+}
+
+public class World
+{
+
+}
+
+public class ConsoleFunctions
+{
+    public static void MainMenu(ref int choice)
+    {
+        Console.WriteLine("Main menu");
+        Console.WriteLine("===============");
+        Console.WriteLine("1 - Open inventory");
+        Console.WriteLine("2 - Open marketplace");
+        Console.WriteLine("3 - Go hunt");
+        Console.WriteLine(" ");
+        Console.Write("Enter what you want to do: ");
+        while (!int.TryParse(Console.ReadLine(), out choice) || choice < 1 || choice > 3)
         {
-            Console.Write("Enter your username: ");
-            player.username = Console.ReadLine();
-        }
-        public static void OptionsDisplay(ref int choice)
-        {
-            Console.WriteLine("Here are some of your options:");
-            Console.WriteLine("1 - Go on adventure");
-            Console.WriteLine("2 - Open shop");
-            Console.WriteLine("3 - Exit the game");
-            Console.Write("Enter your choice: ");
-            choice = int.Parse(Console.ReadLine());
+            Console.WriteLine("Error , please try again");
+            Console.Write("Enter what you want to do: ");
         }
 
-        public static void ActOnChoice(int choice)
+    }
+    
+    public static void HandleChoice(int choice)
+    {
+        switch (choice)
         {
-            switch (choice)
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+
+        }
+    }
+
+    public static void OpenInventory(Player playerInstance)
+    {
+        Console.WriteLine("Your Inventory");
+        foreach (var (item, index) in playerInstance.items.Select((value, i) => (value, i + 1)))
+        {
+            Console.WriteLine($"{index}) {item}");
+        }
+    }
+
+    public static void OpenMarketplace(Player playerInstance)
+    {
+        int choice = -1, lastCrateIndex;
+
+        Console.WriteLine($"Coins: {playerInstance.coins}");
+        Console.WriteLine("");
+        Console.WriteLine("Marketplace");
+        foreach (var (egg, index) in Crate.GetAllCrates())
+        {
+            Console.WriteLine($"{index}) {egg}");
+
+            lastCrateIndex = index;
+        }
+        Console.Write("Enter the number of the crate you want to open (0 to go back to main menu): ");
+        while (!int.TryParse(Console.ReadLine(), out choice) || choice < -1)
+        {
+            Console.WriteLine("Error , please try again");
+            Console.WriteLine("Enter the number of the crate you want to open (0 to go back to main menu): ");
+        }
+    }
+
+    public static void OpenIndex(Player playerInstance) 
+    {
+        foreach (var (weapon, index) in Crate.GetAllWeapons().Select((value, i) => (value, i + 1)))
+        {
+            string discovery = "(Undiscovered)";
+            foreach (var playerweapon in playerInstance.weaponInventory)
             {
-                case 1:
-                    Console.WriteLine();
-                    break;
-                case 2:
-                    Console.WriteLine();
-                    break;
-                case 3:
-                    Console.WriteLine();
-                    break;
+                if (playerweapon == weapon.name)
+                {
+                    discovery = "(Discovered)";
+                }
+            }
+            Console.WriteLine($"{index}) {weapon.name} {discovery}");
+        }
+    }
+}
+
+public class Utils
+{
+    public static string articleCheck(string word)
+    {
+        string[] vowelChars = { "A", "a", "E", "e", "I", "i", "O", "o" };
+        foreach (var character in vowelChars)
+        {
+            if (word.StartsWith(character))
+            {
+                return $"an {word}";
             }
         }
+        return $"a {word}";
+    }
+
+    public static void DebugDisplay(object variable, string description)
+    {
+        Console.WriteLine($"{variable} = {description}");
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Player playerInstance = new Player();
+        ConsoleFunctions.OpenInventory(playerInstance);
+        ConsoleFunctions.OpenMarketplace(playerInstance);
     }
 }
